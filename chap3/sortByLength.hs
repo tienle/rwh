07c8e2e520dc99a76f7997e32aa884f3a5487fc3
Tien Le <tienlx@gmail.com>
2010-03-13 07:14:19 +0700

    sortByLength

---
+++ chap3/sortByLength.hs
@@ -0,0 +1,7 @@
+sortByLength [] = []
+sortByLength x@(_:[]) = x 
+sortByLength xs = let (m, zs) = bubleup xs in (m:sortByLength zs)
+			       where  
+			          bubleup (x:[])                               = (x, [])
+			          bubleup (x:xs:zs) | (length x) > (length xs) = let (m, ys) = bubleup (xs:zs) in (m, x:ys)
+								        | otherwise                = let (m, ys) = bubleup (x:zs) in (m, xs:ys)
